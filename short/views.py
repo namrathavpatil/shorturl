84e8e746cfbb5418eb4637bd64113c17cf982464
namrathavpatil <46816441+namrathavpatil@users.noreply.github.com>
2021-06-02 15:58:31 +0530

    update

--- short/views.py
+++ short/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import User
-from .forms import NameForm, urlForm
+
 import hashlib
 from django.shortcuts import redirect
 from django.http import HttpResponse
@@ -12,31 +12,34 @@ def get_name(request, *args, **kwargs):
     print(request.method)
     # print(form.cleaned_data['your_name'])
     # if this is a POST request we need to process the form data
+    context = {}
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            context = {}
-            a = form.cleaned_data['your_name']
-            print(a)
-            b = hashlib.md5(str(a).encode()).hexdigest()
-            print(a, b)
-            reg = User(name=a, id=b[:3])
 
-            reg.save()
-            l = User.objects.filter(s_name=b).first()
-            context['e'] = b[:3]
+        # check whether it's valid:
+
+        # process the data in form.cleaned_data as required
+        # ...
+        # redirect to a new URL:
+        context = {}
+        a = request.POST['your_name']
+        print(a)
+        b = hashlib.md5(str(a).encode()).hexdigest()
+        print(a, b)
+        reg = User(name=a, id=b[:3])
+        print(request)
+
+        reg.save()
+        l = User.objects.filter(s_name=b).first()
+        # a = (User.objects.get(pk=b[:3]))
+        # b = a.name
+        context['post'] = b[:3]
+
         return render(request, 'name.html', context)
 
     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
 
-    return render(request, 'name.html', {'form': form})
+    return render(request, 'name.html', context)
 
 
 def get_url(request, *args, **kwargs):
@@ -47,7 +50,7 @@ def get_url(request, *args, **kwargs):
         return redirect(a.name)
     except:
         print(request.path[1:])
-        return render(request, 'list.html', {})
+        return render(request, 'name.html', {})
     # create a form instance and populate it with data from the request:
     # if request.method == 'POST':
     #     form = urlForm(request.POST)
