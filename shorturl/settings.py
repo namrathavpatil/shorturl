b10e24f86e0fce6b42aa34f421c222eae3fa7a0a
namrathavpatil <namratha.10.8.2000@gmail.com>
2021-06-09 20:18:07 +0530

    df

--- shorturl/settings.py
+++ shorturl/settings.py
@@ -136,6 +136,3 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'shorturl/static'),
-)
