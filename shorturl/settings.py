6095f2e51a46ed8853e7b3104667aca398e08f89
namrathavpatil <namratha.10.8.2000@gmail.com>
2021-06-09 20:52:06 +0530

    dfd

--- shorturl/settings.py
+++ shorturl/settings.py
@@ -135,7 +135,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, ''),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
