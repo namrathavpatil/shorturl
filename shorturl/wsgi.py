580fe1f7a9fd198d295ae6b967cae0b78659167c
namrathavpatil <namratha.10.8.2000@gmail.com>
2021-06-09 21:39:58 +0530

    df

--- shorturl/wsgi.py
+++ shorturl/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shorturl.settings')
 
 application = Cling(get_wsgi_application())
-application.add_files('/shorturl/static')
